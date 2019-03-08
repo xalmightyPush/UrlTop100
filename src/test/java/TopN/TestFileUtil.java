e5e76329b652cf161c2153654873b67ebb1d4dac
huyabin <huyabin@chinaubi.com>
2019-03-08 16:28:50 +0800

    modify

--- src/test/java/TopN/TestFileUtil.java
+++ src/test/java/TopN/TestFileUtil.java
@@ -1,15 +1,18 @@
 package TopN;
 
-import TopN.Utils.FileUtil;
-import java.io.File;
+import Utils.FileUtil;
 import org.junit.Test;
+
+import java.io.File;
 
 public class TestFileUtil {
     @Test
     public void testFileSplit() {
         try {
-            String targetPath = "D:\\soucecode\\data";
-            File file = new File("D:\\soucecode\\data\\data.txt");
+            /*String targetPath = "D:\\soucecode\\data";
+            File file = new File("D:\\soucecode\\data\\data.txt");*/
+            String targetPath = "D:\\data2\\s0";
+            File file = new File("D:\\data2\\0.part");
             FileUtil fileUtil = new FileUtil();
             fileUtil.splitFile(file, targetPath);
         } catch (Exception e) {
@@ -20,12 +23,14 @@ public class TestFileUtil {
     @Test
     public void testReadSplitFileToWC() {
         try {
-            String sourcePath = "D:\\soucecode\\data";
-            String targetPath = "D:\\soucecode\\data";
+           /* String sourcePath = "D:\\soucecode\\data";
+            String targetPath = "D:\\soucecode\\data";*/
+            String sourcePath = "D:\\data2\\s0";
+            String targetPath = "D:\\data2\\s1";
             File sourceFile = new File(sourcePath);
             File targetFile = new File(targetPath);
             FileUtil fileUtil = new FileUtil();
-            fileUtil.readSplitFileToWC(sourceFile);
+            fileUtil.readSplitFileToWC(sourceFile, targetFile);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -34,12 +39,14 @@ public class TestFileUtil {
     @Test
     public void testFileMerge() {
         try {
-            String sourcePath = "D:\\soucecode\\data";
-            String targetPath = "D:\\soucecode\\data\\cs";
+            /*String sourcePath = "D:\\soucecode\\data";
+            String targetPath = "D:\\soucecode\\data\\cs";*/
+            String sourcePath = "D:\\data2\\s1";
+            String targetPath = "D:\\data2\\s2";
             FileUtil fileUtil = new FileUtil();
             File sourceFile = new File(sourcePath);
             File targetFile = new File(targetPath);
-            fileUtil.mergeFile(sourceFile, targetFile);
+            fileUtil.mergeFile(sourceFile, targetFile, "data.txt");
         } catch (Exception e) {
             e.printStackTrace();
         }
